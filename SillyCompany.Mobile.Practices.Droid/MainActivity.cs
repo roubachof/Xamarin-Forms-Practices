910f3f51bde4675f34fe2e31a141b315f841b2b9
roubachof <jm.alfonsi@gmail.com>
2019-06-15 17:14:55 +0200

    update xf to v4

--- SillyCompany.Mobile.Practices.Droid/MainActivity.cs
+++ SillyCompany.Mobile.Practices.Droid/MainActivity.cs
@@ -7,12 +7,15 @@
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
-
+using Android.Views;
 using Xamarin.Forms.Platform.Android;
 
 namespace SillyCompany.Mobile.Practices.Droid
 {
-    [Activity(Theme = "@style/ZoliTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(
+        Theme = "@style/ZoliTheme",
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        WindowSoftInputMode = SoftInput.AdjustResize)]
     public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
