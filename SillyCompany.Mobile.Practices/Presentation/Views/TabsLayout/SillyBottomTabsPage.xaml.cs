1a6b366cbc8182ec5d51e3d3ad9ff2c5d283cdba
roubachof <jm.alfonsi@gmail.com>
2020-01-16 19:11:41 +0100

    filled empty views with silly things

--- SillyCompany.Mobile.Practices/Presentation/Views/TabsLayout/SillyBottomTabsPage.xaml.cs
+++ SillyCompany.Mobile.Practices/Presentation/Views/TabsLayout/SillyBottomTabsPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Threading.Tasks;
+
+using Sharpnado.Tasks;
+
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace SillyCompany.Mobile.Practices.Presentation.Views.TabsLayout
@@ -10,6 +15,20 @@ namespace SillyCompany.Mobile.Practices.Presentation.Views.TabsLayout
         {
             SetValue(NavigationPage.HasNavigationBarProperty, false);
             InitializeComponent();
+            TabButton.TapCommand = new Command(() => System.Diagnostics.Debug.WriteLine("TapButton tapped!"));
+        }
+
+        private void TabButtonOnClicked(object sender, EventArgs e)
+        {
+            TaskMonitor.Create(AnimateTabButton);
+        }
+
+        private async Task AnimateTabButton()
+        {
+            await TabButton.ScaleTo(2);
+            await TabButton.ScaleTo(1);
+            await TabButton.ScaleTo(2);
+            await TabButton.ScaleTo(1);
         }
     }
 }
